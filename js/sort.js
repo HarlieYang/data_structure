86061faae798eb925e2b3e895ab6c77b2afe9ba6
杨惠 <yanghui3021@163..com>
2020-07-26 19:09:38 +0800

    插入排序

--- js/sort.js
+++ js/sort.js
@@ -1,7 +1,7 @@
 /*
- * @Author: your name
+ * @Author: HarlieYang
  * @Date: 2020-07-24 11:08:29
- * @LastEditTime: 2020-07-24 13:50:20
+ * @LastEditTime: 2020-07-26 18:54:49
  * @LastEditors: Please set LastEditors
  * @Description: 数组排序算法
  * @FilePath: /数据结构/js/sort.js
@@ -11,9 +11,8 @@ class sort {
         this.data = data
     }
 
-    // 冒泡排序
+    // 1. 冒泡排序
     bubbleSort(){
-        console.log('冒泡排序')
         let data = this.data
         for (let i = 0; i<data.length; i++){
             for (let j = i + 1; j < data.length; j++){
@@ -27,6 +26,40 @@ class sort {
         }
         console.log('冒泡排序------',data)
     }
+
+    // 2. 选择排序
+    selectSort(){
+        let data = this.data
+        for (let i = 0; i < data.length; i++){
+            let curIndex = i
+            for(let j = i + 1; j < data.length ; j++){
+                if (data[curIndex] > data[j]) {
+                    curIndex = j
+                }
+            }
+            let temp =  data[i]
+            data[i] = data[curIndex]
+            data[curIndex] = temp
+        }
+        console.log('选择排序------',data)
+    }
+
+    // 3. 插入排序  时间复杂度为O(n^2)  空间复杂度为O(1)
+    insertSort(){
+        let data = this.data
+        for (let i = 1; i < data.length; i++){
+            let current = data[i]
+            let currentIndex = i
+            while(currentIndex > 0 && data[currentIndex -1] > current ){
+                data[currentIndex] = data[currentIndex-1]
+                currentIndex--
+            }
+            data[currentIndex] = current
+        }
+        console.log('插入排序------',data)
+    }
+    
+    
 }
 let a = new sort([1,9,4,7,10,0])
-a.bubbleSort()
+a.insertSort()
